104cdd90983ad1c4bdda3d02f13c1fcc54d80410
tar1z <923321822@qq.com>
2024-03-19 15:02:25 +0800

    feat: 实现jsx打包

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -4,7 +4,7 @@ import {
 	Key,
 	Ref,
 	Props,
-	ReactElement,
+	ReactElementType,
 	ElementType
 } from 'shared/ReactTypes';
 
@@ -13,7 +13,7 @@ const ReactElement = function (
 	key: Key,
 	ref: Ref,
 	props: Props
-): ReactElement {
+): ReactElementType {
 	const element = {
 		$$typeof: REACT_ELEMENT_TYPE,
 		type,
---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,46 @@
+import { getPackageJson, resolvePkgPath, getBaseRollupPlugins } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJson('react');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+  {
+    input: `${pkgPath}/${module}`,
+    output: {
+      file: `${pkgDistPath}/index.js`,
+      name: 'index.js',
+      format: 'umd'
+    },
+    plugins: [
+      ...getBaseRollupPlugins(),
+      generatePackageJson({
+        inputFolder: pkgPath,
+        outputFolder: pkgDistPath,
+        baseContents: ({ name, description, version }) => ({
+          name,
+          description,
+          version,
+          main: 'index.js'
+        })
+      })
+    ]
+  },
+  {
+    input: `${pkgPath}/src/jsx.ts`,
+    output: [
+      {
+        file: `${pkgDistPath}/jsx-runtime.js`,
+        name: 'jsx-runtime.js',
+        format: 'umd'
+      },
+      {
+        file: `${pkgDistPath}/jsx-dev-runtime.js`,
+        name: 'jsx-dev-runtime.js',
+        format: 'umd'
+      }
+    ],
+    plugins: getBaseRollupPlugins()
+  }
+];
